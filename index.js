793435ee5b51d1480b8bc71504e6dade5bf07a2d
Nicolai Ferraris <nicolaifsf@gmail.com>
2015-04-12 07:33:48 -0400

    sweg

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var dispatcher = require('httpdispatcher');
 var express = require('express');
 var _ = require('lodash-node');
 var Tokenizer = require('sentence-tokenizer');
+var mongoose = require('mongoose');
+https://apis.google.com/js/client.js?onload=handleClientLoad
+
+var apiKey = 'AIzaSyAVcy616MHXLq0h2DAMxxnhcmZrZT5OChg';
+
+var scope = 'https://mail.google.com/';
 
 var app = express();
 var http = require('http');
@@ -48,7 +54,7 @@ SummaryTool.summarize(subject,body,function(err,summary){
 	console.log("hit");
 	console.log(summary);
 	res.end(summary);
-	
+
 });
 
 	console.log("hereee");
@@ -291,3 +297,239 @@ exports.getSortedSentences = function(content, n, callback) {
 		});
 	});
 };
+
+
+
+
+
+//-------------------------------------------------------------------------------------------
+
+var userlist = [];//want to store this in db
+
+var userID;
+var loginid;
+
+
+var authToken
+
+//A sample GET request    
+dispatcher.onGet("/api/v1/login", function(req, res) {
+	userID = req.id;
+	authToken = req.authToken;
+	var clientSecret = wo7c7Ap0v5AcGcuN9WvL8ODI;
+
+
+
+});    
+
+
+
+      function handleClientLoad() {
+        // Step 2: Reference the API key
+        gapi.client.setApiKey(apiKey);
+        window.setTimeout(checkAuth,1);
+      }
+
+
+      function checkAuth() {
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+      }
+
+            function handleAuthResult(authResult) {
+        var authorizeButton = document.getElementById('authorize-button');
+        if (authResult && !authResult.error) {
+          authorizeButton.style.visibility = 'hidden';
+          makeApiCall();
+        } else {
+          authorizeButton.style.visibility = '';
+          authorizeButton.onclick = handleAuthClick;
+        }
+      }
+
+
+            function handleAuthClick(event) {
+        // Step 3: get authorization to use private data
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+        return false;
+      }
+
+      // Load the API and make an API call.  Display the results on the screen.
+      function makeApiCall() {
+        // Step 4: Load the Google+ API
+        gapi.client.load('plus', 'v1').then(function() {
+          // Step 5: Assemble the API request
+          var request = gapi.client.plus.people.get({
+            'userId': 'me'
+          });
+          // Step 6: Execute the API request
+          request.then(function(resp) {
+            var heading = document.createElement('h4');
+            var image = document.createElement('img');
+            image.src = resp.result.image.url;
+            heading.appendChild(image);
+            heading.appendChild(document.createTextNode(resp.result.displayName));
+
+            document.getElementById('content').appendChild(heading);
+          }, function(reason) {
+            console.log('Error: ' + reason.result.error.message);
+          });
+        });
+      }
+
+
+//A sample GET request    
+dispatcher.onGet("/page1", function(req, res) {
+    console.log("well, got something? maybe its sweig");
+
+//assume messages is an array with messages at the tag
+    var messages = req.messages;
+
+
+    for(i=0; i<messages.length; ++i){
+   messagelist[messagelist.length] = JSONObj.messages[i].id;
+}
+
+
+// var loginid;//passed in
+ var JSONObj;//passed in
+
+var messagelist = [];
+
+for(i=0; i<messages.length; ++i){
+   messagelist[messagelist.length] = JSONObj.messages[i].id;
+}
+//Passed-in Stuff END
+
+
+
+
+
+//Passed-in Stuff START
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback){
+  
+
+//User Scheme START
+   var userSchema = mongoose.Schema({
+      userID: String,
+      messages: [{msgID:string, priority:int}]
+   })
+   
+   userSchema.methods.speak = function(){
+      var greeting = this.userName
+         ? "ID: : " + this.userID
+         : "I have no name"
+      console.log(greeting);
+   }
+
+   //NEED to get JSON of message IDs from userID
+   //userSchema.methods.refresh = function refresh ( loginid ){}
+   
+   var User = mongoose.model('User', userSchema);
+
+   //var userlistSchema = mongoose.Schema({ users: [] })
+   //var Userlist = mongoose.model('Userlist', userlistSchema);
+});
+//User Scheme END
+
+
+
+
+
+
+
+
+//Adding maybe? new User START
+   var repeat = false;
+   var Fred;
+
+   //Does User exist?
+   for(var x in userlist){
+      if(x.userID == loginid){
+         repeat = true;
+         Fred = x;
+         break;
+      }
+   }
+
+if(!repeat){
+   userID = loginid;
+   messages = [];
+   var Freddy = new User(userID,messages);
+   userlist[userlist.length] = Freddy;
+
+   var importante = 0;
+   for(i=0; i<messagelist.length; ++i){
+    
+       Freddy.messages[Freddy.messages.length] = (messagelist[i], importante);
+   }
+}
+//adding maybe? new user END
+
+
+
+//updating messagelist START
+else{
+   var newm = false;
+   for(i=0; i<messagelist.length; ++i){
+      for(j=0; j<Fred.messages.length; ++j){
+         if(messagelist[i] == Fred.messages[j].msgID) break;
+         if(j==Fred.messages.length) newm=true;
+      }
+      if(newm) break;
+   }
+   if(newm){
+      var newmsgs = [];
+      var importante = 0;
+      for(i=0; i<messagelist.length; ++i){ //Check for messages Fred already has
+         for(j=0; j<Fred.messages.length; ++j){
+            if(messagelist[i] == Fred.messages[j].msgID) break;
+            if(j==Fred.messages.length){ //If message is new
+               
+               newmsgs[newmsgs.length]=(messagelist[i],importante);
+            }
+         }
+      }
+      for(i=0; i<newmsgs.length; ++i){ //Add new messages to Fred
+         Fred.messages[Fred.messages.length]=newmsgs[i];
+      }
+   }
+}
+//updating messagelist END
+
+
+
+//User.find({ userID: /^id/ }, callback)
+//console.log(Fred.userName)
+
+userlist.save(function(err,userlist){
+   if(err) return console.error(err);
+   console.dir(userlist);
+});
+
+/*Array.find(function(err,){
+   if(err) return console.error(err);
+   console.log(userlist);
+})*/
+
+mongoose.connect('mongodb://localhost/test');
+
+
+
+
+
+
+
+
+    res.end('Page One');
+
+
+
+
+
+});    
+
+
